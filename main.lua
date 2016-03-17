d52d3ae36cd29b9ea489e34bbd3ebecda9d73396
dwarfplumber <15123057@studentmail.ul.ie>
2016-03-17 16:08:59 +0000

    set up for exclusive landscape orientation

--- main.lua
+++ main.lua
@@ -18,27 +18,56 @@ axis["Gamepad 1"]["right_x"] = 3
 axis["Gamepad 1"]["right_y"] = 4
 axis["Gamepad 1"]["right_trigger"] = 6
 
-local redPlayer = display.newImage( "TestFace.png" )
-redPlayer:setFillColor( 1, 0, 0 )
-redPlayer.x = display.contentCenterX
-redPlayer.y = display.contentCenterY
-redPlayer.isMovingX = 0
-redPlayer.isMovingY = 0
-redPlayer.isRotatingX = 0
-redPlayer.isRotatingY = 0
-redPlayer.thisAngle = 0
-redPlayer.rotationDistance = 0
-redPlayer.isGrowing = 0
-redPlayer.color = "red"
-redPlayer.velocity = 10
+local player = display.newImage( "TestFace.png" )
+player:setFillColor( 1, 0, 0 )
+player.x = display.contentCenterX
+player.y = display.contentCenterY
+player.isMovingX = 0
+player.isMovingY = 0
+player.isRotatingX = 0
+player.isRotatingY = 0
+player.thisAngle = 0
+player.rotationDistance = 0
+player.isGrowing = 0
+player.color = "red"
+player.velocity = 10
 
-local rightThumbstick = display.newCircle( display.contentCenterX, display.contentCenterY, 14 )
-rightThumbstick:setFillColor( 1 )
-rightThumbstick.alpha = 0.5
 
-local rightThumbstickBase = display.newCircle( display.contentCenterX, display.contentCenterY, 20 )
+-- Setting up display thumbsticks
+
+local rightThumbstick = display.newGroup()
+
+local rightThumbstickHead = display.newCircle( 0, 0, 14 )
+rightThumbstickHead:setFillColor( 1 )
+rightThumbstickHead.alpha = 0.5
+
+local rightThumbstickBase = display.newCircle( 0, 0, 20 )
 rightThumbstickBase:setFillColor( 1 )
 rightThumbstickBase.alpha = 0.5
+
+rightThumbstick:insert( rightThumbstickBase )
+rightThumbstick:insert( rightThumbstickHead )
+
+local leftThumbstick = display.newGroup()
+
+local leftThumbstickHead = display.newCircle( 0, 0, 14 )
+leftThumbstickHead:setFillColor( 1 )
+leftThumbstickHead.alpha = 0.5
+
+local leftThumbstickBase = display.newCircle( 0, 0, 20 )
+leftThumbstickBase:setFillColor( 1 )
+leftThumbstickBase.alpha = 0.5
+
+leftThumbstick:insert( leftThumbstickBase )
+leftThumbstick:insert( leftThumbstickHead )
+
+
+rightThumbstick.x = display.contentWidth/2 + 50
+rightThumbstick.y = display.contentHeight - 200
+leftThumbstick.x = display.contentWidth/2 - 50
+leftThumbstick.y = display.contentHeight - 200
+
+
 
 -- Calculate the angle to rotate the square. Using simple right angle math, we can
 -- determine the base and height of a right triangle where one point is 0,0
@@ -67,7 +96,7 @@ local function calculateAngle( sideX, sideY )
 
     local angle
     if ( math.abs( sideX ) < 0.1 and math.abs( sideY ) < 0.1 ) then
-        angle = redPlayer.thisAngle
+        angle = player.thisAngle
     elseif ( sideX == 0 ) then
         if ( sideY < 0 ) then
             angle = 0
@@ -99,8 +128,6 @@ local function calculateAngle( sideX, sideY )
             angle = 90 - math.abs( angle )
         end
     end
-
-    print( "angle = " .. angle )
     return angle
 end
 
@@ -110,18 +137,18 @@ end
 
 -- We can also calculate our rotation angle here
 
-local function moveRedPlayer()
+local function moveplayer()
 
     -- Set the .isMovingX and .isMovingY values in our event handler
     -- If this number isn't 0 (stopped moving), move the player
-    if ( redPlayer.isMovingX ~= 0 ) then
-        redPlayer.x = redPlayer.x + redPlayer.isMovingX
+    if ( player.isMovingX ~= 0 ) then
+        player.x = player.x + player.isMovingX
     end
-    if ( redPlayer.isMovingY ~= 0 ) then
-        redPlayer.y = redPlayer.y + redPlayer.isMovingY
+    if ( player.isMovingY ~= 0 ) then
+        player.y = player.y + player.isMovingY
     end
 
-    redPlayer.rotation = redPlayer.thisAngle
+    player.rotation = player.thisAngle
 end
 
 local function onAxisEvent( event )
@@ -133,10 +160,8 @@ local function onAxisEvent( event )
     -- Map event data to simple variables
     local abs = math.abs
     local controller = event.device.descriptor
-    print( controller )
     local thisAxis = event.axis.number
-    print( thisAxis )
-    local thisPlayer = redPlayer
+    local thisPlayer = player
 
     -- Check which controller this is coming from; you can trust the names
     -- "Joystick 1" and "Joystick 2" to represent player 1, player 2, etc.
@@ -161,6 +186,7 @@ local function onAxisEvent( event )
            thisPlayer.isMovingX = 0
        end
 
+       leftThumbstickHead.x = event.normalizedValue * 10
 
     elseif ( axis[controller]["left_y"] == thisAxis ) then
 
@@ -172,27 +198,25 @@ local function onAxisEvent( event )
            thisPlayer.isMovingY = 0
        end
 
-       
+       leftThumbstickHead.y = event.normalizedValue * 10
 
     elseif ( axis[controller]["right_x"] == thisAxis ) then
 
         -- We will use the right stick to rotate our player
         thisPlayer.isRotatingX = event.normalizedValue
-        print( "X rotation" .. thisPlayer.isRotatingX)
         thisPlayer.thisAngle = math.floor( calculateAngle(thisPlayer.isRotatingX, thisPlayer.isRotatingY) )
 
         -- Draw the blue dot around the center to show how far you actually moved the stick
-       rightThumbstick.x = display.contentCenterX + event.normalizedValue * 10
+       rightThumbstickHead.x = event.normalizedValue * 10
 
     elseif ( axis[controller]["right_y"] == thisAxis ) then
 
         -- Repeat for the Y axis on the right stick
         thisPlayer.isRotatingY = event.normalizedValue
-        print( "Y rotation" .. thisPlayer.isRotatingY)
         thisPlayer.thisAngle = math.floor( calculateAngle(thisPlayer.isRotatingX, thisPlayer.isRotatingY) )
 
         -- Move the blue dot
-       rightThumbstick.y = display.contentCenterY + event.normalizedValue * 10
+       rightThumbstickHead.y = event.normalizedValue * 10
 
     elseif ( axis[controller]["left_trigger"] or axis[controller]["right_trigger"] == thisAxis ) then
 
@@ -268,9 +292,19 @@ end
 local function onKeyEvent( event )
    local phase = event.phase
    local keyName = event.keyName
-   print( event.phase, event.keyName )
    local message = "Key '" .. event.keyName .. "' was pressed " .. event.phase
    myKeyDisplayText.text = message
+
+    if (event.phase == "down") then
+        if ( event.keyName == "[" or event.keyName == "rightShoulderButton1" ) then
+            if (player.velocity > 0 ) then
+                player.velocity = player.velocity - 1
+            end
+        elseif ( event.keyName == "]" or event.keyName == "leftShoulderButton1" ) then
+            player.velocity = player.velocity + 1
+        end
+    end
+
    return false
 end
 
@@ -288,4 +322,4 @@ Runtime:addEventListener( "key", onKeyEvent )
 
 Runtime:addEventListener( "axis", onAxisEvent )
 
-Runtime:addEventListener( "enterFrame", moveRedPlayer )
+Runtime:addEventListener( "enterFrame", moveplayer )
