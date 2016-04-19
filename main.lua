1c7cef97676ab0b760717f2e59c0d4596ec73220
david159ryan <0867284@studentmail.ul.ie>
2016-04-19 17:27:32 +0100

    pushing

--- main.lua
+++ main.lua
@@ -1,7 +1,8 @@
 local g = require "globals"
+native.setProperty("windowMode", "fullscreen")
 if(system.getInfo("platformName") == "Android") then
     display.setStatusBar( display.HiddenStatusBar )
     system.activate( "multitouch" )
 end
 local composer = require "composer"
-composer.gotoScene( g.scenePath.."intro" )
+composer.gotoScene( g.scenePath.."game" )
